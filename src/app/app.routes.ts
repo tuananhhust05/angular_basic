c36d2ef1c19f616aac2e29a10697285542d44ce2
tuananhhust05 <tuananhhust05@gmail.com>
2024-07-15 21:01:30 +0700

    update

---
+++ src/app/app.routes.ts
@@ -0,0 +1,43 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { ParentcategoryComponent } from './pages/parentcategory/parentcategory.component';
+import { ChildCategoryComponent } from './pages/child-category/child-category.component';
+
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full',
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'',
+        component:LayoutComponent,
+        children: [
+            {
+                path:'dashboard',
+                component:DashboardComponent
+            },
+            {
+                path:'department',
+                component:DepartmentComponent
+            },
+            {
+                path:'parent-category',
+                component:ParentcategoryComponent
+            },
+            {
+                path:'child-category',
+                component:ChildCategoryComponent
+            },
+           
+        ]
+    }
+];
